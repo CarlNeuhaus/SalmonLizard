2d447ae9d82ad84f44cfa829578dc29bf196baca
root <root@localhost.localdomain>
2015-02-23 17:37:07 +1100

    update

--- framework/core.py
+++ framework/core.py
@@ -3,6 +3,7 @@
 import os
 
 from framework.utilities import Namespace, Filewalker
+from framework.template import LizardModule
 
 class Framework(object):
 
@@ -18,12 +19,13 @@ class Framework(object):
     print "Searching for modules in: " + modules_path
     self.current_module = None
     if not os.path.isdir(modules_path):
-      print 'path to modules not found'
+      #print 'path to modules not found'
+      pass
     for module_path in Filewalker(modules_path, absolute_path=True, skip_dirs=True):
       module_path = module_path.replace(os.path.sep, '/')
       if not module_path.endswith('.py'):
         continue
-      print module_path
+      #print module_path
       module_path = module_path[len(modules_path):-3]
       module_name = module_path.split(os.path.sep)[-1]
       if module_name.startswith('__'):
@@ -34,9 +36,11 @@ class Framework(object):
       try:
         module_instance = self.import_module(module_path)
       except:
-        print "failed"
+        #print "failed"
+        pass
       if not hasattr(module_instance, 'run'):
-        print ('module: ' + module_path + ' has no run() method')
+        #print ('module: ' + module_path + ' has no run() method')
+        pass
       module_instance.name = module_name
       module_instance.path = module_path
       self.modules[module_path] = module_instance
@@ -46,8 +50,9 @@ class Framework(object):
   def import_module(self, module_path, reload_module=False):
     try:
       module = __import__(self.__package__ + '.modules.' + module_path.replace('/', '.'), None, None, ['Module'])
-      #module_instance = module.Module(self)
-      module_instance = module
+      print module
+      module_instance = module.Module(self)
+      #module_instance = module
     except Exception as err:
       message = 'failed to load module: ' + module_path
       print message 
---
+++ framework/template.py
@@ -0,0 +1,4 @@
+class LizardModule(object):
+  def __init__(self, frmwk):
+    self.frmwk = frmwk
+    self.name = 'unknown'
