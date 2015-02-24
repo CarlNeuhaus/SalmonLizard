de2526d8df1225e8037bfc50c6719fd13ed1ebb8
root <root@localhost.localdomain>
2015-02-24 17:40:56 +1100

    threading

--- framework/modules/ftp_login.py
+++ framework/modules/ftp_login.py
@@ -23,19 +23,27 @@ from framework.template import LizardModule
 import ftplib
 import time
 from framework.formatting import coloursLinux as colours
+import thread
 
 class Module(LizardModule):
+
     def run(self):
-      hostname = "192.168.253.128"
-      self.anonLogin(hostname)
+      hostnames = ["192.168.253.128","192.168.253.127","192.168.253.127","192.168.253.127","192.168.253.127","192.168.253.127","192.168.253.127","192.168.253.127","192.168.253.127","192.168.253.127"]
+      for hostname in hostnames:
+        try:
+          thread.start_new_thread(self.anonLogin, (hostname,))
+        except Exception, e:
+          print e
+      return
+
     def anonLogin(self, hostname):
-      try:
-        ftp = ftplib.FTP(hostname)
-        ftp.login('anonymous', 'me@you.com')
-        print colours.OKGREEN + '\n[*] ' + str(hostname) + ' FTP Anonymous Logon Succeeded\n' + colours.ENDC
-        ftp.quit()
-      except Exception, e:
-        print colours.WARNING + '\n[-] ' + str(hostname) + ' FTP Anonymous Logon Failed\n' + colours.ENDC
+        try:
+          ftp = ftplib.FTP(hostname)
+          ftp.login('anonymous', 'me@you.com')
+          print colours.OKGREEN + '\n[*] ' + str(hostname) + ' FTP Anonymous Logon Succeeded\n' + colours.ENDC
+          ftp.quit()
+        except Exception, e:
+          print colours.WARNING + '\n[-] ' + str(hostname) + ' FTP Anonymous Logon Failed\n' + colours.ENDC
   
 
      
